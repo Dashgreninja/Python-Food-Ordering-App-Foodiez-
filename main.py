4d43b110129d47a460b073d439c670472cb14473
Dashgreninja <68371315+Dashgreninja@users.noreply.github.com>
2023-04-05 09:53:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from foodiez_functions import Main
+from foodiez_functions import *
 
+class Main:
+    def execute(self,log_choice):
+        if log_choice == 1:
+            log.admin_login()
+                
+        elif log_choice == 2:
+            
+            while True:
+                print("\n+---------------------------------------+\n|Enter 1 to Register                    | \n|Enter 2 to Login if already Registered |\n+---------------------------------------+")
+                reg_choice = int(input("Enter Your Choice :- "))
+                if reg_choice == 1:
+                    log.register()
+                elif reg_choice == 2:
+                    log.user_login()
+                else:
+                    print("Invalid Choice")
+                
+
+        else:
+            print('See You Soon ')
+            exit()
+     
                 
 print("                                          Welcome To Foodiez")
 print("+--------------------------+\n|Enter 1 to Login as Admin | \n|Enter 2 to Login as User  |\n+--------------------------+\n")
